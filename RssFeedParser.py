889404367456a788f5b613018798aa358207adf3
brainmurphy1 <brianmurphy@gatech.edu>
2014-09-05 18:33:51 -0400

    add rss feed parser

---
+++ RssFeedParser.py
@@ -0,0 +1,17 @@
+import feedparser
+
+etag  = None
+class RssLinkParser(object):
+    def __init__(self, rss_url):
+        self.etag = None
+        self.rss_url = rss_url
+
+    def get_links(self):
+        feed = feedparser.parse(self.rss_url, etag = self.etag)
+        if 'etag' in feed:
+            self.etag = feed.etag
+        links = []
+        print feed.entries[0].keys()
+        for entry in feed.entries:
+            links.append(entry.link)
+        return links
