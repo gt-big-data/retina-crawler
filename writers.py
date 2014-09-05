ec1a72789a76faa0e5d5c7750117981d68a7673d
Sam Marder <sam@marder.co>
2014-09-05 18:46:11 -0400

    Added sample writers for viewing a JSONable object.

---
+++ writers.py
@@ -0,0 +1,29 @@
+import json
+
+class PrintWriter(object):
+	"""Class for writing JSON data to the screen."""
+	def __init__(self):
+		pass
+
+	def write(self, json_data):
+		"""Write a JSON serializable object to the screen.
+
+		Arguments:
+		json_data -- A JSON serializable object.
+		"""
+		print json.dumps(json_data, indent=4, ensure_ascii=False)
+
+class FileWriter(object):
+	"""Class for writing JSON data to a file."""
+	def __init__(self, filepath):
+		self._filepath = filepath
+
+	def write(self, json_data):
+		"""Write a JSON serializable object to a file.
+
+		Arguments:
+		json_data -- A JSON serializable object.
+		"""
+		pretty_string = json.dumps(json_data, indent=4, ensure_ascii=False)
+		with open(self._filepath, 'w') as output_file:
+			output_file.write(pretty_string)
