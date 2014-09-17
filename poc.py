6307975b31b33a981cd6c1a243062c94c4fef489
Sam Marder <sam@marder.co>
2014-09-17 19:40:53 -0400

    Asks for a URL instead of putting the link parsing code in the main method.

--- poc.py
+++ poc.py
@@ -2,9 +2,17 @@ import article
 from writers import PrintWriter, FileWriter
 from RssFeedParser import RssLinkParser
 import hashlib
+import newspaper
+import feedfinder
 
-def main():
-	feed_parser = RssLinkParser("http://rss.cnn.com/rss/edition.rss")
+def get_all_the_feeds():
+	for url in newspaper.popular_urls():
+		rss_feeds = feedfinder.feeds(url)
+		for rss_feed in rss_feeds:
+			process_url(rss_feed)
+
+def process_url(url):
+	feed_parser = RssLinkParser(url)
 	links = feed_parser.get_links()
 	
 	for link in links:
@@ -13,6 +21,9 @@ def main():
 		filename = hashlib.md5(article_data["title"]).hexdigest() + ".txt"
 		writer = FileWriter(filename)
 		writer.write(article_data)
+
+def main():
+	process_url("http://rss.cnn.com/rss/edition.rss")
 
 if __name__ == "__main__":
 	main()
