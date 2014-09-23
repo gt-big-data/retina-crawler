7a4cd5c8fabbd7646edbef7382e74072851e4388
Daryl Halima <INTERN@lawn-128-61-126-215.lawn.gatech.edu>
2014-09-23 19:15:13 -0400

    Add error logging

--- poc.py
+++ poc.py
@@ -2,17 +2,26 @@ import article
 from writers import PrintWriter, FileWriter
 from RssFeedParser import RssLinkParser
 import hashlib
+import logging
+import time
 
 def main():
-	feed_parser = RssLinkParser("http://rss.cnn.com/rss/edition.rss")
-	links = feed_parser.get_links()
-	
-	for link in links:
-		article_info = article.create_article(link)
-		article_data = article.article_dictionary(article_info)
-		filename = hashlib.md5(article_data["title"]).hexdigest() + ".txt"
-		writer = FileWriter(filename)
-		writer.write(article_data)
+
+	while True:
+		feed_parser = RssLinkParser("http://rss.cnn.com/rss/edition.rss")
+		try:
+			links = feed_parser.get_links()
+			
+			for link in links:
+				article_info = article.create_article(link)
+				article_data = article.article_dictionary(article_info)
+				filename = hashlib.md5(article_data["title"]).hexdigest() + ".txt"
+				writer = FileWriter(filename)
+				writer.write(article_data)
+		except Exception, e:
+			logging.error(str(e))
+			time.sleep(0.5)
+		
 
 if __name__ == "__main__":
 	main()
