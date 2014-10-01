f6e0785f913eabe47069173ba389106180471bbe
Patrick Violette <violette.patrick718@gmail.com>
2014-10-01 15:56:17 -0400

    Added configurable runners

--- poc.py
+++ poc.py
@@ -1,41 +1,40 @@
-import article
-from writers import PrintWriter, FileWriter, MongoWriter
-from ArticleInserter import ArticleInserter
-from RssFeedParser import RssLinkParser
-import hashlib
+import CrawlerRunners
+import sys
+import json
 import logging
 import time
 
-feeds = ["http://rss.cnn.com/rss/edition.rss", # world/national news
-		"http://feeds.abcnews.com/abcnews/topstories",
-		"http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml",
-		"http://www.ajc.com/list/rss/news/local/news-georgia-and-region/aCxP/", # local news		
-		"http://www.reddit.com/.rss", # entertainment
-		"http://www.tmz.com/rss.xml",
-		"http://sports.espn.go.com/espn/rss/news", # sports
-		"http://online.wsj.com/xml/rss/3_7455.xml", # finance
-		"http://feeds.feedburner.com/TechCrunch/", #tech
-		"http://podcasts.engadget.com/rss.xml",
-		"http://feeds.feedburner.com/hacker-news-feed?format=xml",
-		]
+logging.basicConfig(level=logging.INFO)
+logger = logger = logging.getLogger('retina-crawler')
 
 def main():
-	article_writer = MongoWriter()
-	feed_parsers = [RssLinkParser(feed) for feed in feeds]
-	while True:
-		for feed_parser in feed_parsers:
-			try:
-				links = feed_parser.get_new_links()
-				
-				for link in links:
-					article_info = article.create_article(link)
-					article_data = article.article_dictionary(article_info)
-					article_writer.write(article_data)
-					time.sleep(0.1) # So we don't get blocked
-			except Exception, e:
-				logging.error(str(e))
-				time.sleep(0.5)
-		
+    runner = None
+    logger.info('RETINA CRAWLER starting..')
+    try:
+        if len(sys.argv) < 2:
+            runner = CrawlerRunners.SimpleRunner()
+        else:
+            config = None
+            config_file_path = sys.argv[1]
+            with open(config_file_path) as f:
+                config = json.load(f)
+            runner_type = getattr(CrawlerRunners, config['runner'])
+            logger.info('loaded runner "{}"'.format(config['runner']))
+            if 'args' in config:
+                runner = runner_type(config['args'])
+            else:
+                runner = runner_type()
+    except Exception, e:
+        logger.error('Invalid config given, error is "{}"'.format(str(e)))
+        sys.exit(-1)
 
+    while True:
+        try:
+            logger.info('Running at {}'.format(time.time()))
+            runner.run()
+        except Exception, e:
+            logger.error(str(e))
+            time.sleep(0.5)
+        
 if __name__ == "__main__":
-	main()
+    main()
