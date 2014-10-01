d939a1fada4eb7ef6f3c3d02edd2d563c3887268
Sam Marder <sam@marder.co>
2014-10-01 19:39:22 -0400

    Integrated multithreading with new Runners model.

--- main.py
+++ main.py
@@ -2,18 +2,48 @@ from RssFeedParser import RssLinkParser
 from downloaders import *
 from writers import *
 
+import CrawlerRunners
+import sys
+import json
+import logging
+import time
+
+logging.basicConfig(level=logging.INFO)
+logger = logger = logging.getLogger('retina-crawler')
+
 def find_articles(url):
-	feed_parser = RssLinkParser(url)
-	links = feed_parser.get_links()
-	return links
+    feed_parser = RssLinkParser(url)
+    links = feed_parser.get_links()
+    return links
 
 def main():
-	links = find_articles("http://rss.cnn.com/rss/edition.rss")
-	downloader = MultiProcessDownloader(FileWriter())
-	for link in links:
-		downloader.queue_link(link)
+    runner = None
+    logger.info('RETINA CRAWLER starting..')
+    try:
+        if len(sys.argv) < 2:
+            runner = CrawlerRunners.SimpleRunner()
+        else:
+            config = None
+            config_file_path = sys.argv[1]
+            with open(config_file_path) as f:
+                config = json.load(f)
+            runner_type = getattr(CrawlerRunners, config['runner'])
+            logger.info('loaded runner "{}"'.format(config['runner']))
+            if 'args' in config:
+                runner = runner_type(config['args'])
+            else:
+                runner = runner_type()
+    except Exception, e:
+        logger.error('Invalid config given, error is "{}"'.format(str(e)))
+        sys.exit(-1)
 
-	downloader.download_all()
+    while True:
+        try:
+            logger.info('Running at {}'.format(time.time()))
+            runner.run()
+        except Exception, e:
+            logger.error(str(e))
+            time.sleep(0.5)
 
 if __name__ == "__main__":
-	main()
+    main()
