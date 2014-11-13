0cd4ece3415571b58783658eb2a01393fbe81fb4
Sam Marder <sam.eclipse@marder.co>
2014-11-12 19:38:21 -0500

    Added sanity checks for missing basic information (summary, pub_date).
    Added an additional check to try finding the published date.

--- article.py
+++ article.py
@@ -3,18 +3,6 @@ from datetime import datetime
 import parsers
 from urlparse import urlparsefrom lxml.html.clean import clean_htmlfrom lxml.html import document_fromstring
 
-
-parser_lookup = [
-    ('cnn.com', parsers.parse_cnn_article),
-    ('nytimes', parsers.parse_nytimes_article),
-]
-
-def _getParserForUrl(url):
-    for domain, parser in parser_lookup:
-        if domain in url:
-            return parser
-    return parsers.newspaper_parser
-
 class Article(object):
 
     def to_dict(self):
@@ -39,12 +27,10 @@ class Article(object):
         self.pub_date = None
         self.html = None
         self._parsed = False
-        self._parser = parser or _getParserForUrl(url)
 
     def download_and_parse(self):
         if self._parsed:
             raise Exception('This article ({}) has already been parsed.'.format(self.url))        self.download_date = datetime.now()        self.source_domain = urlparse(self.url).netloc        self._parsed = True        try:
             self.html = requests.get(self.url).content
         except requests.exceptions.RequestException:
-            raise IOError("Could not download the article at: %s" % self.url)        # This alters the html in-place.        clean_html(self.html)        if (self._parser == parsers.newspaper_parser):            self._parser(self)        else:            doc = document_fromstring(self.html)
-            self._parser(self, doc)
+            raise IOError("Could not download the article at: %s" % self.url)        # This alters the html in-place.        clean_html(self.html)        doc = document_fromstring(self.html)        parsers.parse_article(self, doc)
