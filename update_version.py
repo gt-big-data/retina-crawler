0ba56d65d8f8d7b86ce7a1dfb399219164e8f5d5
Matt Ersted <mersted@gatech.edu>
2015-02-18 19:39:07 -0500

    ability to display crawler version as doc in db :100:

---
+++ update_version.py
@@ -0,0 +1,24 @@
+import pymongo
+
+class UpdateVersion():
+
+    def __init__(self):
+        self.m = pymongo.MongoClient("146.148.59.202", 27017)
+        self.db = self.m.big_data
+
+    def add_version_number(self, number):
+        doc = {"_id" : "version",
+                "number" : number}
+        self.db.articles.insert(doc)
+
+    def update_version_number(self, number):
+        query = {"_id" : "version"}
+        self.db.articles.update(query, {"number" : number})
+
+    def get_version_number(self):
+        query = {"_id" : "version"}
+        doc = self.db.articles.find_one(query)
+        return doc["number"]
+
+u = UpdateVersion()
+u.add_version_number("0.0.7")
